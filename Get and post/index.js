30fab137003352064a1c0514612bcf5f38e6ef94
jawwad9 <jawwadmuhammad514@gmail.com>
2024-11-11 02:00:25 +0500

    first commit

--- Get and post/index.js
+++ Get and post/index.js
@@ -3,8 +3,21 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')  
+  res.send('Hello World!')
+  
 })
+
+
+app.get('/About', (req, res) => {
+    res.send('Hello About!')
+    
+  })
+
+  
+app.get('/service', (req, res) => {
+    res.send('Hello service!')
+    
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
