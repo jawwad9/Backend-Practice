131d3e15700b523b041db8ee3fdeab0cabf34990
jawwad9 <jawwadmuhammad514@gmail.com>
2024-11-17 16:24:31 +0500

    complete

--- Get and post Api/index.js
+++ Get and post Api/index.js
@@ -3,9 +3,22 @@ import express from "express";
 const app = express()
 const port = 3000
 
+
+const users = [{
+    id: 1,
+    name: "John",
+}];
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/', (req, res) => {
+    res.send('users')
+  })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
