dfc008bdcdaaeaca0541fb137f3867eabbbf2bd4
jawwad9 <jawwadmuhammad514@gmail.com>
2024-11-27 23:25:40 +0500

    complete

--- Mongoos-db/src/controllers/todo.controllers.js
+++ Mongoos-db/src/controllers/todo.controllers.js
@@ -1,10 +1,9 @@
 import todosModels from "../models/todos.models.js";
 
 // add todo 
-
 const addTodo = async (req, res) => {
     const { title, description } = req.body;
-    if (!title || !description) return res.status(400).json({ message: "Title And Description Is Required" });
+    if (!title || !description) return res.status(400).json({ message: "Title And Description Is Required"});
 
     await todosModels.create({ title, description });
     res.status(201).json({ message: "Todo Added success in the db" });
