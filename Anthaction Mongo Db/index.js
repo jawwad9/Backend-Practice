107aec2cc9ff1169ff0a63925f09193e0636acd3
jawwad9 <jawwadmuhammad514@gmail.com>
2024-12-01 23:26:33 +0500

    complete bcrypt

--- Anthaction Mongo Db/index.js
+++ Anthaction Mongo Db/index.js
@@ -2,20 +2,38 @@ import express from "express";
 import dotenv from "dotenv"
 dotenv.config();
 import connectDb from "./src/db/index.js";
+import bcrypt from "bcrypt"
+import cors from "cors"
 
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("hello world!")
 })
 
+// bcrypt password
+const encrypt = "$2b$10$eza2WdScTzrO2uyW7qqhl.ll2W1ugvN4zO1pVsf.p4uoObsjsNnI."
 
+
+// bcrypt password create
+app.post("/encryptpassword", (req, res) => {
+    const { password } = req.body;
+    bcrypt.hash(password, 10, function(err, hash) {
+        if (err) return res.status(402).json({ message: "password error"});
+        res.json({ password: hash })
+    });
+})
+
+
+
+// MONGO DB and Server connection
 connectDb().then(() => {
 app.listen(process.env.PORT, () => {
-    console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+    console.log(`Server is running at port : ${process.env.PORT}`);
 })
 }).catch((err) => {
     console.log("MONGO DB connection failed !!! ", err);
--- Mongoos-db/src/routes/todos.routes.js
+++ Mongoos-db/src/routes/todos.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addTodo } from "../controllers/todos.controllers.js";
+import { addTodo } from "../controllers/todo.controllers.js";
 
 const router = express.Router();
 
